28fa36ba65a0333c4e02e9381ab09dd60c21f71a
Arka2008 <71698336+Arka2008@users.noreply.github.com>
2020-11-19 14:58:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//Create variables here
+var dog,happyDog;
+var database;
+var foodS,foodStock;
+
+function preload()
+{
+  //load images here
+  dog.image=loadImage(dogImg.png);
+  happyDog.image.loadImage(dogImg1.png);
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
